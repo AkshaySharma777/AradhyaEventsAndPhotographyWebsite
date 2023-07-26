2eea99babfd89cac2d47b7ef289eae8eaf12e852
AkshaySharma777 <jsakshay653@gmail.com>
2023-07-26 19:13:51 +0530

    Started initial HTML, CSS and JS along with assets

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+// Form submission
+const detailsForm = document.getElementById('detailsForm');
+
+detailsForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Retrieve form field values
+  const fullname = document.getElementById('fullname').value;
+  const mobile = document.getElementById('mobile').value;
+  const email = document.getElementById('email').value;
+  const purpose = document.getElementById('purpose').value;
+
+  // Perform desired actions with the form data
+  // This could include saving to a database, sending emails, etc.
+
+  // Validate the mobile number
+  if (mobile.length !== 10 || isNaN(mobile)) {
+    alert('Please enter a valid 10-digit mobile number.');
+    return;
+  }
+    // Validate the email address
+      // Validate the email address
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+    if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+  // Example action: Redirect to a new page
+  window.location.href = 'mainPage.html';
+});
